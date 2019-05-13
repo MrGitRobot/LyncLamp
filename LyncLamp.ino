ef313b4d5371e8245878f88cc5e985717cec4c99
christian.schuller <christian.schuller@bshg.com>
2019-05-13 15:37:19 +0200

    added pink "kicker" state

--- LyncLamp.ino
+++ LyncLamp.ino
@@ -7,22 +7,25 @@
 #define STATE_GREEN 1
 #define STATE_ORANGE 2
 #define STATE_RED 3
+#define STATE_PINK 4
 #define STATE_NONE 0
 
 //State Colors
 #define COLOR_GREEN 1
 #define COLOR_ORANGE 2
 #define COLOR_RED 3
+#define COLOR_PINK 4
 
 //Button Pins
 #define GREEN_BTN_PIN 2
 #define ORANGE_BTN_PIN 3
 #define RED_BTN_PIN 4
+#define PINK_BTN_PIN 5
 
 //Time over LED pin
 #define CONCENTRATION_TIME_OVER_LED_PIN 8
 
-//Status LED ring 
+//Status LED ring
 #define STATUS_LED_PIN 6
 #define NUM_LEDS 12
 #define BRIGHTNESS 20
@@ -32,10 +35,12 @@ Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_LEDS, STATUS_LED_PIN, NEO_GRBW +
 bool greenBtnPressed = 0;
 bool orangeBtnPressed = 0;
 bool redBtnPressed = 0;
+bool pinkBtnPressed = 0;
 
 bool lastGreenBtnPressed = 0;
 bool lastOrangeBtnPressed = 0;
 bool lastRedBtnPressed = 0;
+bool lastPinkBtnPressed = 0;
 
 long concentrationTime = 0;
 long lastConcentrationTime = 0;
@@ -53,6 +58,7 @@ void setup() {
   pinMode(GREEN_BTN_PIN, INPUT_PULLUP);
   pinMode(ORANGE_BTN_PIN, INPUT_PULLUP);
   pinMode(RED_BTN_PIN, INPUT_PULLUP);
+  pinMode(PINK_BTN_PIN, INPUT_PULLUP);
   pinMode(CONCENTRATION_TIME_OVER_LED_PIN, OUTPUT);
 
   strip.setBrightness(BRIGHTNESS);
@@ -128,6 +134,7 @@ int checkUserInput() {
   bool greenBtnPressed = isPressed(GREEN_BTN_PIN, &lastGreenBtnPressed);
   bool orangeBtnPressed = isPressed(ORANGE_BTN_PIN, &lastOrangeBtnPressed);
   bool redBtnPressed = isPressed(RED_BTN_PIN, &lastRedBtnPressed);
+  bool pinkBtnPressed = isPressed(PINK_BTN_PIN, &lastPinkBtnPressed);
 
   if (greenBtnPressed) {
     Serial.println("Green pressed");
@@ -144,6 +151,11 @@ int checkUserInput() {
     return STATE_RED;
   }
 
+  else if (pinkBtnPressed) {
+    Serial.println("Pink pressed");
+    return STATE_PINK;
+  }
+
   //no button pressed
   else {
     return STATE_NONE;
@@ -151,70 +163,86 @@ int checkUserInput() {
 }
 
 
-int changeState(int reqState,  bool hasPermissions){
-  if(hasPermissions){
-    if(reqState == STATE_NONE)
+int changeState(int reqState,  bool hasPermissions) {
+  if (hasPermissions) {
+    if (reqState == STATE_NONE)
       return lastUserState;
     else
       return reqState;
   }
 
-  else{ //no permissions
-    if(reqState == STATE_GREEN){
+  else { //no permissions
+    if (reqState == STATE_GREEN) {
       return STATE_GREEN;
     }
 
-    else if(reqState == STATE_ORANGE){
+    else if (reqState == STATE_ORANGE) {
       return STATE_ORANGE;
     }
 
-    else if(reqState == STATE_RED){
+    else if (reqState == STATE_PINK) {
+      return STATE_PINK;
+    }
+
+    else if (reqState == STATE_RED) {
       return lastUserState;
     }
 
-    else{ //STATE_NONE
-      if(userState == STATE_RED)
+    else { //STATE_NONE
+      if (userState == STATE_RED)
         return STATE_GREEN;
       else
         return userState;
-      
+
     }
   }
 }
 
 
-void setStatusLEDs(int colorSelector){
-  for(int i = 0; i<strip.numPixels(); i++){
-    if(colorSelector == COLOR_GREEN){
-      strip.setPixelColor(i, 0,255,0,0);
+void setStatusLEDs(int colorSelector) {
+  for (int i = 0; i < strip.numPixels(); i++) {
+    if (colorSelector == COLOR_GREEN) {
+      strip.setPixelColor(i, 0, 255, 0, 0);
     }
-    else if(colorSelector == COLOR_ORANGE){
-      strip.setPixelColor(i, 255,255,0,0);
+    else if (colorSelector == COLOR_ORANGE) {
+      strip.setPixelColor(i, 255, 255, 0, 0);
     }
-    else{ //COLOR_RED
-      strip.setPixelColor(i, 255,0,0,0);
+
+    else if (colorSelector == COLOR_PINK) {
+      strip.setPixelColor(i, 255, 0, 255, 0);
+    }
+    
+    else { //COLOR_RED
+      strip.setPixelColor(i, 255, 0, 0, 0);
     }
   }
   strip.show();
 }
 
 
-void showState(int state){
-  if(state == STATE_GREEN){
+void showState(int state) {
+  if (state == STATE_GREEN) {
     setStatusLEDs(COLOR_GREEN);
   }
-  else if (state == STATE_ORANGE){
+  else if (state == STATE_ORANGE) {
     setStatusLEDs(COLOR_ORANGE);
   }
-  else{ //STATE_RED
+
+  else if (state == STATE_PINK) {
+    setStatusLEDs(COLOR_PINK);
+  }
+  
+  else { //STATE_RED
     setStatusLEDs(COLOR_RED);
   }
 }
 
-void showConcentrationTimeOver(bool isOverTime){
-  if(isOverTime)
+void showConcentrationTimeOver(bool isOverTime) {
+  if (isOverTime)
     digitalWrite(CONCENTRATION_TIME_OVER_LED_PIN, HIGH);
   else
     digitalWrite(CONCENTRATION_TIME_OVER_LED_PIN, LOW);
 }
-  
+
+
+
